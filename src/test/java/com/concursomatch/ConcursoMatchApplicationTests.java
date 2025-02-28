b66909d29e79e95c0cb73490e5a38eeda1fcee46
Jhonata Demuner <jhonata.demuner@gmail.com>
2025-02-27 22:12:04 -0300

    quality: add explanation comment

--- src/test/java/com/concursomatch/ConcursoMatchApplicationTests.java
+++ src/test/java/com/concursomatch/ConcursoMatchApplicationTests.java
@@ -8,6 +8,9 @@ class ConcursoMatchApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// This test ensures that the Spring application context loads successfully.
+		// It is left empty intentionally, as the presence of the @SpringBootTest annotation
+		// is enough to validate that the application starts without issues.
 	}
 
 }
