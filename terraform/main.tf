578a25bfb231124b6f3713963e9c285f34aea176
Jhonata Demuner <jhonata.demuner@gmail.com>
2025-03-09 13:44:50 -0300

    fix: add ec2 role

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,18 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "LedsChallengeEc2Profile"
+  role = "leds-devops-challenge-ec2-access"
+}
+
 resource "aws_instance" "leds-devops" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.key_name
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
   user_data = <<-EOF
               #!/bin/bash
